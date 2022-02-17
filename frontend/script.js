3d7537c51c37136e7fd6f7dc6aefb2623bd708ea
Kriszti <kecskeskriszti22@gmail.com>
2022-02-17 18:35:18 +0100

    practice done

--- frontend/script.js
+++ frontend/script.js
@@ -16,52 +16,37 @@ function loadEvent() {
     };
 
     let renderAllCardElements = function (incomingMoviesArray) {
-        let renderedCardList = "";
+        let renderedCardList = `<div class="cards">`;
          // for ciklus ami végigmegy a listán, a cardsarrayen
         for (const incomingMovie of incomingMoviesArray) {
+
+            renderedCardList += card2(incomingMovie.title, incomingMovie.year, incomingMovie.rate)
+
             // for ciklus minden lépcsőjénél hozzáadja a rendercardlisthez az adott elemet a megfelelő div card-ban
-            renderedCardList += `
+            /* renderedCardList += `
             <div class="card">
                 <h2>${incomingMovie.title}</h2>
                 <div class="time">Year: ${incomingMovie.year}</div>
                 <div class="rate">Rate: ${incomingMovie.rate}</div>
-            </div>`
+            </div>` */
         };
+        renderedCardList += `</div>`
 
         console.log(renderedCardList);
         // returneli az elkészült elemekkel feltöltött rendelt card listet.
         return renderedCardList;
     }
 
-    // movies.sort(function(a,b){return a.year - b.year});
-    // console.log(movies);
-
     let newGoodMovies = [];
 
     for (const movieSend of movies) {
 
-        // let floorRate = Math.floor(movieSend.rate);
-
-
-
         if (movieSend.year > 2000 && movieSend.rate >= 8) {
             newGoodMovies.push(movieSend);
-            /* rootElement.insertAdjacentHTML("beforeend", card2(movieSend.title, movieSend.year, floorRate)); */
         }
-
-        /* let newerThan2000 = false;
-
-        if (movieSend.year > 2000) {
-            newerThan2000 = true
-        }
-
-        if (newerThan2000) {
-            rootElement.insertAdjacentHTML("beforeend", card2(movieSend.title, movieSend.year, movieSend.rate));
-        } */
     };
 
     newGoodMovies.sort(function(a,b){return a.year - b.year});
-    // console.log(newGoodMovies);
     rootElement.insertAdjacentHTML("beforeend", renderAllCardElements(newGoodMovies));
 
 };
