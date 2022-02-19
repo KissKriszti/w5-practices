64599373d7999cf461263272dd7cf3f858f97612
Kriszti <kecskeskriszti22@gmail.com>
2022-02-19 14:02:32 +0100

    updated for grid showcase

--- frontend/script.js
+++ frontend/script.js
@@ -6,20 +6,23 @@ function loadEvent() {
 
     let rootElement = document.getElementById("root");
 
+    let szamlalo = 0
     let card2 = function (title,year,rate){
+            szamlalo++
             return `
-            <div class="card">
+            <div class="card card${szamlalo}">
                 <h2>${title}</h2>
                 <div class="time">Year: ${year}</div>
                 <div class="rate">Rate: ${rate}</div>
             </div>`;
     };
 
+
     let renderAllCardElements = function (incomingMoviesArray) {
         let renderedCardList = `<div class="cards">`;
          // for ciklus ami végigmegy a listán, a cardsarrayen
         for (const incomingMovie of incomingMoviesArray) {
-
+            
             renderedCardList += card2(incomingMovie.title, incomingMovie.year, incomingMovie.rate)
 
             // for ciklus minden lépcsőjénél hozzáadja a rendercardlisthez az adott elemet a megfelelő div card-ban
@@ -33,7 +36,7 @@ function loadEvent() {
         renderedCardList += `</div>`
 
         console.log(renderedCardList);
-        // returneli az elkészült elemekkel feltöltött rendelt card listet.
+        // returneli az elkészült elemekkel feltöltött renderelt card listet.
         return renderedCardList;
     }
 
@@ -41,13 +44,18 @@ function loadEvent() {
 
     for (const movieSend of movies) {
 
-        if (movieSend.year > 2000 && movieSend.rate >= 8) {
+        if (movieSend.year > 2015 && movieSend.rate >= 8) {
             newGoodMovies.push(movieSend);
         }
     };
 
     newGoodMovies.sort(function(a,b){return a.year - b.year});
     rootElement.insertAdjacentHTML("beforeend", renderAllCardElements(newGoodMovies));
+
+    // necessary for the grid introduction
+    /* document.getElementsByClassName("card")[0].classList.add("card1");
+
+    document.getElementsByClassName("card")[1].classList.add("card2") */
 
 };
 
